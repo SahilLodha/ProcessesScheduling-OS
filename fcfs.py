3aa0dfd573e69fd8dfd1f1387b60942930e81047
sahillodha <sahil.lodha0421@gmail.com>
2021-03-28 19:04:07 +0545

    Changed fcfs.py.

--- fcfs.py
+++ fcfs.py
@@ -1,18 +1,9 @@
-from main import Process
+from main import Process, inputFunction
 import sys
 
-processes = []
-while True:
-    temp = Process()
-    processes.append(temp)
-    flag = str(input('Want to continue[Y/N]: ')).lower()
-    if flag == 'n':
-        break
-
+processes = inputFunction()
 print("The entered Information is provided below:")
-for proc in processes:
-    print(proc)
-    print("<--------- Next --------->")
+Process.print_array(processes)
 
 flag = input("Is it correct [Y/N]: ").lower()
 if flag == 'n':
@@ -35,11 +26,7 @@ for proc in processes:
     end_time_last = proc.endTime
     proc.leftovers = 0
 
-for proc in processes:
-    print(proc)
-    print(f"Start Time: {proc.startTime}")
-    print("<--------- Next --------->")
-
+Process.print_proc_with_leftover(processes)
 print(f'Hence the average waiting time is {Process.avg_waiting_time_FCFS(processes):.2f}')
 print(f'The CPU Utilization is {sum([proc.burstTime for proc in processes])/processes[-1].endTime:.4f}')
 print(f"The turn around time is {sum(proc.endTime - proc.startTime for proc in processes)/len(processes)}")
